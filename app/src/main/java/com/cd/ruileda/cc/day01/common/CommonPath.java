6bdbb6370717cc1561e7f613ae7ffa48cff89534
evan <562581802@qq.com>
2021-04-24 13:59:27 +0800

    修改room，优化完成

--- app/src/main/java/com/cd/ruileda/cc/day01/common/CommonPath.java
+++ app/src/main/java/com/cd/ruileda/cc/day01/common/CommonPath.java
@@ -6,7 +6,8 @@ import java.io.File;
 
 public class CommonPath {
 
-    public static final String ROOT_DIR = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "1_hospital_face";
+    public static final String ROOT_DIR = Environment.getExternalStorageDirectory().getAbsolutePath() +
+            File.separator + "1_table_farm";
     public static final String TABLE_DIR = ROOT_DIR + File.separator + "table";
 
 
